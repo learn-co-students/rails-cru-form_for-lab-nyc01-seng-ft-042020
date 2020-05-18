568deccd9d29a6eec2a7c3edb23bd9cffc0ff7a2
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-05-18 19:39:13 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Delete
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+#Artists
+
+artist1 = Artist.create(name: "Metallica", bio: "Metallica's Bio.")
+artist2 = Artist.create(name: "Sublime", bio: "Sublime's Bio.")
+
+#Genres
+
+genre1 = Genre.create(name: "Rock")
+genre2 = Genre.create(name: "Punk")
+
+#Songs
+
+song1 = Song.create(name: "2 by 4", artist_id: artist1.id, genre_id: genre1.id)
+song2 = Song.create(name: "Santaria", artist_id: artist2.id, genre_id: genre2.id)
+song3 = Song.create(name: "What I Got", artist_id: artist2.id, genre_id: genre2.id)
