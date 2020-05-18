69a88f8546174b158f5010ac573aeb726df3aec1
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-05-17 21:28:33 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Artist.delete_all
+Song.delete_all
+Genre.delete_all
+
+10.times do
+    Artist.create(name: Faker::Artist.name,bio: Faker::GreekPhilosophers.quote)
+
+end
+
+10.times do
+    Genre.create(name: Faker::Music.genre)
+end
+
+puts "done"
+
